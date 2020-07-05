4952ecb32bf1f7349d41e12af3c7acb37b23d8d2
Bee Bones <gelbundschwarz@gmail.com>
2020-07-05 19:16:03 -0300

    adicionado react no projeto

---
+++ frontend/src/data.js
@@ -0,0 +1,48 @@
+export default {
+    products:[{
+        name: 'Camisa Nike Coreia',
+        category: 'Shirts',
+        image: '/images/nike_coreia.jpg',
+        price: 200.00,
+        brand: 'Nike',
+        rating: 4.5,
+        numReviews: 10
+    },
+    {
+        name: 'Camisa Skeletor + Gatinhos',
+        category: 'Shirts',
+        image: '/images/camisa_skeletor.jpg',
+        price: 150.00,
+        brand: 'Meow',
+        rating: 5,
+        numReviews: 80
+    },
+    {
+        name: 'Camisa Animal Print Onça',
+        category: 'Shirts',
+        image: '/images/camisa_onca.jpg',
+        price: 180.00,
+        brand: 'BAW',
+        rating: 4,
+        numReviews: 10
+    },
+    {
+        name: 'Camisa Animal Print Zebra',
+        category: 'Shirts',
+        image: '/images/camisa-zebra-01.jpg',
+        price: 180.00,
+        brand: 'BAW',
+        rating: 4,
+        numReviews: 10
+    },
+    {
+        name: 'Camisa Spooky Tunes',
+        category: 'Shirts',
+        image: '/images/spooky_tunes.jpg',
+        price: 200.00,
+        brand: 'Killstar',
+        rating: 5,
+        numReviews: 100
+    }
+]
+}
